a70b842ac99e749c76fa0bade04b10abb76588bc
derickmacharia66-jpg <derickmacharia66@gmail.com>
2026-09-06 21:24:19 +0300

    Implement book and coffee classes with properties and methods

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title: str, page_count: int):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self) -> int:
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value: int):
+        if isinstance(value, int) and not isinstance(value, bool):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+            self._page_count = None
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
     
         
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size: str, price: float | int):
+        self.size = size
+        self.price = price
+
+    @property
+    def size(self) -> str:
+        return self._size
+
+    @size.setter
+    def size(self, value: str):
+        valid_sizes = ["Small", "Medium", "Large"]
+        if value in valid_sizes:
+            self._size = value
+        else:
+            print('size must be Small, Medium, or Large')
+            self._size = None
+
+    def tip(self):
+        print("This coffee is great, here's a tip!")
+        if self.price is not None:
+            self.price += 1
+    
